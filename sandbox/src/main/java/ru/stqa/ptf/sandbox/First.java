46c41da1fbf8917b82b14bf7a6c5fea93f85a09f
vafl9 <thebestvafl9@gmail.com>
2016-08-14 19:23:21 +0300

    Добавил пару функций

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
@@ -1,8 +1,26 @@
 package ru.stqa.ptf.sandbox;
 
-public class First{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, world!");
-	}
+public class First {
+    public static void main(String[] args) {
+        printHello("Hello");
+        printHello("Hello");
+        printHello("Hello");
+        int i = 3;
+        System.out.println(area(i));
+        int b = 4;
+        System.out.println(area(i,b));
+    }
+
+
+    public static void printHello(String value) {
+        System.out.println(value);
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
