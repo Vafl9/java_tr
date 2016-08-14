716ef326a59154dc0f7125cdde8b4f4a6360a175
vafl9 <thebestvafl9@gmail.com>
2016-08-14 20:07:45 +0300

    Выполнил задание

--- sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
+++ sandbox/src/main/java/ru/stqa/ptf/sandbox/First.java
@@ -2,25 +2,14 @@ package ru.stqa.ptf.sandbox;
 
 public class First {
     public static void main(String[] args) {
-        printHello("Hello");
-        printHello("Hello");
-        printHello("Hello");
-        int i = 3;
-        System.out.println(area(i));
-        int b = 4;
-        System.out.println(area(i,b));
+        Point p1 = new Point(2, 2);
+        Point p2 = new Point(3, 3);
+        System.out.println(distance(p1, p2));
+
+        System.out.println(p1.distance(p1,p2));
     }
 
-
-    public static void printHello(String value) {
-        System.out.println(value);
-    }
-
-    public static double area(double l) {
-        return l * l;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
     }
 }
