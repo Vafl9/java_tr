279764ed5a8d57ca2c2bef3ae2fdb97809b6b7b8
vafl9 <thebestvafl9@gmail.com>
2016-10-22 15:52:14 +0300

    Teh com

---
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/DBconnectionTest.java
@@ -0,0 +1,38 @@
+package ru.stqa.ptf.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.ptf.addressbook.model.GroupDate;
+import ru.stqa.ptf.addressbook.model.Groups;
+
+import java.sql.*;
+
+public class DBconnectionTest {
+
+    @Test
+    public void TestDBconnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/addressbook?user=root&password=&serverTimezone=UTC");
+
+            Statement st = conn.createStatement();
+            ResultSet resultSet = st.executeQuery("SELECT * FROM group_list ");
+
+            Groups group =  new Groups();
+            while (resultSet.next())
+            {
+                group.add(new GroupDate().withName(resultSet.getString("group_name")).withId(resultSet.getInt("group_id")));
+            }
+            resultSet.close();
+            st.close();
+            conn.close();
+            System.out.println(group);
+            // Do something with the Connection
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+}
