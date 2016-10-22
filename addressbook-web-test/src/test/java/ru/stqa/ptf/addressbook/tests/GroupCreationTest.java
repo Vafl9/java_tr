aa555035da2d1f8be28303ee0fd080d86832cdde
vafl9 <thebestvafl9@gmail.com>
2016-10-22 15:03:13 +0300

    teh com

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTest.java
@@ -3,6 +3,8 @@ package ru.stqa.ptf.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupDate;
@@ -55,6 +57,8 @@ public class GroupCreationTest extends TestBase {
             return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
         }
     }
+
+
 
     @Test(dataProvider = "validGroupsXML")
     public void groupCreationTest(GroupDate group) {
--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/TestBase.java
@@ -1,12 +1,22 @@
 package ru.stqa.ptf.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.ptf.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 public class TestBase {
 
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
@@ -14,9 +24,22 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite (alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+
+    @BeforeMethod
+    public void logTestStart(Method method, Object[] objects)
+    {
+        logger.info("Start test " + method.getName()+ Arrays.asList(objects));
+    }
+
+    @AfterMethod (alwaysRun = true)
+    public void logTestStop(Method method)
+    {
+        logger.info("Stop test " + method.getName());
     }
 
 }
