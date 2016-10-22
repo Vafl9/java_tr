82a911851ba09287531a70fed7d0f020b3500333
vafl9 <thebestvafl9@gmail.com>
2016-10-22 11:57:28 +0300

    Teh com

--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/ContactCreationTest.java
@@ -4,9 +4,11 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.ContactDate;
 import ru.stqa.ptf.addressbook.model.Contacts;
-import ru.stqa.ptf.addressbook.model.GroupDate;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,12 +20,17 @@ public class ContactCreationTest extends TestBase {
 
 
     @DataProvider
-    public Iterator<Object[]> validContacts(){
+    public Iterator<Object[]> validContacts() throws IOException {
         File photo = new File("src/test/resources/stru.png");
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[] {new ContactDate().withName("Andrew1").withLastName( "Dzhodzhua1").withEmail("Head1@mail.ru").withPhoto(photo)});
-        list.add(new Object[] {new ContactDate().withName("Andrew2").withLastName( "Dzhodzhua2").withEmail("Head2@mail.ru").withPhoto(photo)});
-        list.add(new Object[] {new ContactDate().withName("Andrew3").withLastName( "Dzhodzhua3").withEmail("Head3@mail.ru").withPhoto(photo)});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null)
+        {
+            String[] split = line.split(";");
+            list.add(new Object[]{new ContactDate().withName(split[0]).withLastName(split[1]).withEmail(split[2]).withPhoto(photo)});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
--- addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/ptf/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.ptf.addressbook.model.GroupDate;
 import ru.stqa.ptf.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,16 @@ import static org.hamcrest.junit.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups(){
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<>();
-        list.add(new Object[] {new GroupDate().withName("name1").withHeader("header1").withHeader("footer1")});
-        list.add(new Object[] {new GroupDate().withName("name2").withHeader("header2").withHeader("footer3")});
-        list.add(new Object[] {new GroupDate().withName("name3").withHeader("header2").withHeader("footer3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line != null)
+        {
+            String[] split = line.split(";");
+            list.add(new Object[]{new GroupDate().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
@@ -30,7 +36,7 @@ public class GroupCreationTest extends TestBase {
         app.group().create(group);
         assertThat(app.group().getGroupCount(), equalTo(before.size() + 1));
         Groups after = app.group().all();
-        assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt()))));
+        assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
     @Test
